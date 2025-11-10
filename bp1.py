b2a5521942df5b6524c927a5ac10b58ae59a2bf3
unkonown <unkonownxxd2@gmail.com>
2025-11-10 17:58:13 +0530

    Refactor SparkContext and RDD operations in bp1.py

--- bp1.py
+++ bp1.py
@@ -1,21 +1,21 @@
-# Import SparkContext from pyspark
+#bp1
 from pyspark import SparkContext
 
-# a. Create a SparkContext object
-sc = SparkContext("local", "WordCountApp")  # Initialize SparkContext with local mode and app name
 
-# b. Create RDD using parallelize() from list of words
+sc = SparkContext("local", "WordCountApp") 
+
+
 words = ["scala", "java", "hadoop", "spark", "akka", "spark vs hadoop", "pyspark", "pyspark and spark"]
-rdd = sc.parallelize(words)  # Distribute the list into an RDD
+rdd = sc.parallelize(words) 
 
-# c. Find total count of words in RDD
-total_count = rdd.count()  # Action: returns number of elements in RDD
-print("Word count is %d" % total_count)  # Print total count
 
-# d. Filter strings containing "spark" and collect results
-spark_words_rdd = rdd.filter(lambda x: "spark" in x.lower())  # Transformation: filter rows with "spark"
-spark_words = spark_words_rdd.collect()  # Action: bring filtered data to driver
-print("Filtered RDD: %s" % spark_words)  # Print filtered list
+total_count = rdd.count() 
+print("Word count is %d" % total_count)  
 
-# Stop the SparkContext
-sc.stop()  # Gracefully stop the context
+
+spark_words_rdd = rdd.filter(lambda x: "spark" in x.lower())  
+spark_words = spark_words_rdd.collect() 
+print("Filtered RDD: %s" % spark_words) 
+
+
+sc.stop() 
