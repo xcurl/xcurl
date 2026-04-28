d6fdb1ddfb078cbd8e4b34764e2ab6b012e79564
unkonown <unkonownxxd2@gmail.com>
2026-04-28 08:25:42 +0530

    Add Word Count MapReduce implementation in Java

---
+++ Exp7.java
@@ -0,0 +1,84 @@
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.Reducer;
+
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.TextOutputFormat;
+
+class WC_Mapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(LongWritable key, Text value,
+                    OutputCollector<Text, IntWritable> output,
+                    Reporter reporter) throws IOException {
+
+        String line = value.toString();
+        StringTokenizer tokenizer = new StringTokenizer(line);
+
+        while (tokenizer.hasMoreTokens()) {
+            word.set(tokenizer.nextToken());
+            output.collect(word, one);
+        }
+    }
+}
+
+class WC_Reducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+
+    public void reduce(Text key, Iterator<IntWritable> values,
+                       OutputCollector<Text, IntWritable> output,
+                       Reporter reporter) throws IOException {
+
+        int sum = 0;
+
+        while (values.hasNext()) {
+            sum += values.next().get();
+        }
+
+        output.collect(key, new IntWritable(sum));
+    }
+}
+
+public class WC_Runner {
+
+    public static void main(String[] args) throws IOException {
+
+        JobConf conf = new JobConf(WC_Runner.class);
+
+        conf.setJobName("WordCount");
+
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
+
+        conf.setMapperClass(WC_Mapper.class);
+        conf.setCombinerClass(WC_Reducer.class);
+        conf.setReducerClass(WC_Reducer.class);
+
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
+
+        FileInputFormat.setInputPaths(conf, new Path("input.txt"));
+        FileOutputFormat.setOutputPath(conf, new Path("output"));
+
+        JobClient.runJob(conf);
+    }
+}
