db2a84be62109f3c45d898f08ac7f5afb3ffa35b
unkonown <unkonownxxd2@gmail.com>
2026-04-28 08:27:28 +0530

    Refactor Word Count classes into separate files

--- Exp7.java
+++ Exp7.java
@@ -1,29 +1,18 @@
+//WC_Mapper.java
+
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.StringTokenizer;
 
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 
 import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.Mapper;
-import org.apache.hadoop.mapred.Reducer;
-
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
 
-import org.apache.hadoop.mapred.FileInputFormat;
-import org.apache.hadoop.mapred.FileOutputFormat;
-
-import org.apache.hadoop.mapred.JobClient;
-import org.apache.hadoop.mapred.JobConf;
-
-import org.apache.hadoop.mapred.TextInputFormat;
-import org.apache.hadoop.mapred.TextOutputFormat;
-
-class WC_Mapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+public class WC_Mapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
 
     private final static IntWritable one = new IntWritable(1);
     private Text word = new Text();
@@ -42,7 +31,21 @@ class WC_Mapper extends MapReduceBase implements Mapper<LongWritable, Text, Text
     }
 }
 
-class WC_Reducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+
+//WC_Reducer.java
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+public class WC_Reducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
 
     public void reduce(Text key, Iterator<IntWritable> values,
                        OutputCollector<Text, IntWritable> output,
@@ -57,6 +60,24 @@ class WC_Reducer extends MapReduceBase implements Reducer<Text, IntWritable, Tex
         output.collect(key, new IntWritable(sum));
     }
 }
+
+
+//WC_Runner.java
+
+import java.io.IOException;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.TextOutputFormat;
 
 public class WC_Runner {
 
