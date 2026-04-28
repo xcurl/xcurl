78a4e0764ab98ac97b9d0b9d7f0e10a2e2914eb1
unkonown <unkonownxxd2@gmail.com>
2026-04-28 08:29:40 +0530

    Implement Word Count MapReduce with Mapper and Reducer

---
+++ Exp8.java
@@ -0,0 +1,101 @@
+//WC_Mapper.java
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+public class WC_Mapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+
+    public void map(LongWritable key, Text value,
+                    OutputCollector<Text, IntWritable> output,
+                    Reporter reporter) throws IOException {
+
+        String line = value.toString();
+        String tokenizer[] = line.split("");
+
+        for (String SingleChar : tokenizer) {
+            Text charKey = new Text(SingleChar);
+            IntWritable One = new IntWritable(1);
+            output.collect(charKey, One);
+        }
+    }
+}
+
+
+//WC_Reducer.java
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+public class WC_Reducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+
+    public void reduce(Text key, Iterator<IntWritable> values,
+                       OutputCollector<Text, IntWritable> output,
+                       Reporter reporter) throws IOException {
+
+        int sum = 0;
+
+        while (values.hasNext()) {
+            sum += values.next().get();
+        }
+
+        output.collect(key, new IntWritable(sum));
+    }
+}
+
+//WC_Runner.java
+
+import java.io.IOException;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.TextOutputFormat;
+
+public class WC_Runner {
+
+    public static void main(String[] args) throws IOException {
+
+        JobConf conf = new JobConf(WC_Runner.class);
+
+        conf.setJobName("CharCount");
+
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
+
+        conf.setMapperClass(WC_Mapper.class);
+        conf.setCombinerClass(WC_Reducer.class);
+        conf.setReducerClass(WC_Reducer.class);
+
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
+
+        FileInputFormat.setInputPaths(conf, new Path("input.txt"));
+        FileOutputFormat.setOutputPath(conf, new Path("output"));
+
+        JobClient.runJob(conf);
+    }
+}
